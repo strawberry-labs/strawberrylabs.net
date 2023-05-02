2073a64e977051cac097e71edad26a603cc2c972
Windsmith <shriramsekar11@gmail.com>
2023-05-02 23:13:22 +0400

    feat: implement aws mail sending

--- src/api/saveData.js
+++ src/api/saveData.js
@@ -1,6 +1,13 @@
 import { Timestamp } from 'mongodb';
 import mongoose, { Schema } from 'mongoose';
 
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+
+// Set the AWS Region.
+const REGION = "us-east-1";
+// Create SES service object.
+const sesClient = new SESClient({ region: REGION });
+
 const contactFormSchema = Schema({
     'industry': String,
     'name': String,
@@ -11,15 +18,66 @@ const contactFormSchema = Schema({
     'nda': Boolean
 })
 
+const createSendEmailCommand = (toAddress, fromAddress) => {
+    return new SendEmailCommand({
+        Destination: {
+            /* required */
+            CcAddresses: [
+                /* more items */
+            ],
+            ToAddresses: [
+                toAddress,
+                /* more To-email addresses */
+            ],
+        },
+        Message: {
+            /* required */
+            Body: {
+                /* required */
+                Html: {
+                    Charset: "UTF-8",
+                    Data: "<h1>Hi</h1> <br><p> This is a test email. Have a nice day. </p><br>Kind regards,<br>Strawberry Labs.",
+                },
+                Text: {
+                    Charset: "UTF-8",
+                    Data: "TEXT_FORMAT_BODY",
+                },
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data: "Test Email",
+            },
+        },
+        Source: fromAddress,
+        ReplyToAddresses: [
+            /* more items */
+        ],
+    });
+};
+
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@strawberrylabs.conqfzr.mongodb.net/strawberrylabs?retryWrites=true&w=majority`);
 
 const ContactForm = mongoose.model('contactform', contactFormSchema)
 
 export default async function handler(req, res) {
 
+    /*
+    const doc = new ContactForm(JSON.parse(req.body));
+    await doc.save()
+    */
+    const sendEmailCommand = createSendEmailCommand(
+        "hi@chiragasarpota.com",
+        "notification@strawberrylabs.net"
+    );
 
-    const testDoc = new ContactForm(JSON.parse(req.body));
-    await testDoc.save()
+    try {
+        return await sesClient.send(sendEmailCommand);
+        console.log('done')
+    } catch (e) {
+        console.error("Failed to send email.");
+        console.error(e)
+        return e;
+    }
 
     res.status(200)
 
