fa1bf21240f86ae807e6a7a3bbbaf332834a8cee
Windsmith <shriramsekar11@gmail.com>
2023-06-30 21:14:24 +0400

    fix: remove empty ids for gtag

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -61,13 +61,10 @@ const config: GatsbyConfig = {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           "G-BPPPSZ6JHH", // Google Analytics / GA
-          "", // Google Ads / Adwords / AW
-          "", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
-          optimize_id: "",
           anonymize_ip: true,
           cookie_expires: 0,
         },
