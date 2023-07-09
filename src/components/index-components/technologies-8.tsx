c80ea00c41d26bd9cd229c2dcec1da7ae1c8f26a
Windsmith <shriramsekar11@gmail.com>
2023-07-09 18:28:19 +0400

    fix: technologies buttons scrollable horizontally on mobile

--- src/components/index-components/technologies-8.tsx
+++ src/components/index-components/technologies-8.tsx
@@ -59,29 +59,21 @@ const techData = [
 ]
 
 export default function Section7() {
-    const [techFilter, setTechFilter] = useState("")
+    const [techFilter, setTechFilter] = useState("frontend")
 
     return (
         <Box id="TechStack">
             <Text fontSize={{ base: '3xl', lg: '5xl' }} mt={{ base: 6, lg: 12 }} mx={{ base: 10, lg: 20 }}>Strawberry Labs' set of technologies for software development</Text>
             <Text fontSize={{ base: 'md', lg: 'xl' }} mx={{ base: 10, lg: 20 }} mt={{ base: 4, lg: 8 }}>Cooperate with competent and knowledgeable IT experts in desktop, web, and mobile software engineering. As a trusted tech and business partner, we apply the most advanced technologies, frameworks, and components to build software notable for both its solid and effective back-end with intuitive UI/UX.</Text>
-            <HStack spacing={{ base: 2, lg: 5 }} mx={{ base: 10, lg: 20 }} mt={{ base: 6, lg: 12 }}>
-                <Button onClick={() => setTechFilter('')} isActive={techFilter === "" ? true : false}>All</Button>
-                <Button onClick={() => setTechFilter('frontend')} isActive={techFilter === "frontend" ? true : false}>Frontend</Button>
-                <Button onClick={() => setTechFilter('backend')} isActive={techFilter === "backend" ? true : false}>Backend</Button>
-                <Button onClick={() => setTechFilter('mobile')} isActive={techFilter === "mobile" ? true : false}>Mobile</Button>
-                <Button onClick={() => setTechFilter('database')} isActive={techFilter === "database" ? true : false}>Database</Button>
+            <HStack spacing={{ base: 2, lg: 5 }} mx={{ base: 10, lg: 20 }} mt={{ base: 6, lg: 12 }} overflowX={'scroll'}>
+                <Button onClick={() => setTechFilter('frontend')} isActive={techFilter === "frontend" ? true : false} minW={20}>Frontend</Button>
+                <Button onClick={() => setTechFilter('backend')} isActive={techFilter === "backend" ? true : false} minW={20}>Backend</Button>
+                <Button onClick={() => setTechFilter('mobile')} isActive={techFilter === "mobile" ? true : false} minW={20}>Mobile</Button>
+                <Button onClick={() => setTechFilter('database')} isActive={techFilter === "database" ? true : false} minW={20}>Database</Button>
             </HStack>
             <Box display={{ base: 'none', 'lg': 'block' }}>
                 <SimpleSlider slidesToShow={4} variableWidth={true} arrows={true} horizontalMargin={14}>
-                    {techFilter === "" ? techData.map((item) => (
-                        <Box mx={5} my={1} maxH='60'>
-                            <VStack shadow={'base'} p={14} w={52}>
-                                <Image mb={6} boxSize={20} src={item.image} />
-                                <Link>{item.name}</Link>
-                            </VStack>
-                        </Box>
-                    )) :
+                    {
                         techData.filter((item) => item.type.includes(techFilter)).map((item) => (
                             <Box mx={5} my={1} maxH='60'>
                                 <VStack shadow={'base'} p={14} w={52}>
@@ -93,25 +85,18 @@ export default function Section7() {
                 </SimpleSlider>
             </Box>
             <Box display={{ base: 'block', 'lg': 'none' }}>
-                <SimpleSlider slidesToShow={2} variableWidth={true} arrows={false} horizontalMargin={0}>
-                    {techFilter === "" ? techData.map((item) => (
-                        <Box mx={2} my={1} maxH='28'>
-                            <VStack shadow={'base'} p={14} w={44}>
-                                <Image mb={6} boxSize={20} src={item.image} />
-                                <Link>{item.name}</Link>
-                            </VStack>
-                        </Box>
-                    )) :
+                <SimpleSlider slidesToShow={1} variableWidth={true} arrows={false} horizontalMargin={0}>
+                    {
                         techData.filter((item) => item.type.includes(techFilter)).map((item) => (
-                            <Box mx={2} my={1} maxH='28'>
-                                <VStack shadow={'base'} p={14} w={44}>
-                                    <Image mb={6} boxSize={20} src={item.image} />
+                            <Box mx={2} my={1} maxH={48}>
+                                <VStack shadow={'base'} p={7} w={40} >
+                                    <Image mb={6} boxSize={16} src={item.image} />
                                     <Link>{item.name}</Link>
                                 </VStack>
                             </Box>
                         ))}
                 </SimpleSlider>
-            </Box>
-        </Box>
+            </Box >
+        </Box >
     )
 }
