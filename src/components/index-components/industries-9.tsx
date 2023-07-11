016a88d3e32c29d2f69d30a8bd2116e01dce844d
Windsmith <shriramsekar11@gmail.com>
2023-07-11 16:29:06 +0400

    feat: add icons to the industries

--- src/components/index-components/industries-9.tsx
+++ src/components/index-components/industries-9.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import {
     Box,
     Text,
@@ -7,41 +7,51 @@ import {
     Button
 } from '@chakra-ui/react';
 
-import { ArrowForwardIcon } from "@chakra-ui/icons";
+import { ArrowForwardIcon, Icon } from "@chakra-ui/icons";
+
+import { FaMoneyBillTrendUp, FaHospital, FaFilm, FaPhone, FaTruck, FaSellsy } from 'react-icons/fa6'
 
 import SimpleSlider from "../slider";
+import { IconType } from 'react-icons';
 
 const data = [
     {
         title: "Financial Services",
-        desc: "Thanks to our expertise and track record in Finance, we are a cost-effective and trusted vendor of custom-built software for companies of different sizes and in different jurisdictions."
+        desc: "Thanks to our expertise and track record in Finance, we are a cost-effective and trusted vendor of custom-built software for companies of different sizes and in different jurisdictions.",
+        icon: FaMoneyBillTrendUp
     },
     {
         title: "Healthcare",
-        desc: "As a team of HealthTech experts, we ensure usability, compliance, data security, improved efficiency, interoperability, and the best value for money for healthcare providers and medical researchers."
+        desc: "As a team of HealthTech experts, we ensure usability, compliance, data security, improved efficiency, interoperability, and the best value for money for healthcare providers and medical researchers.",
+        icon: FaHospital
     },
     {
         title: "Media & Entertainment",
-        desc: "Strawberry Labs provides its customers from the Media & Entertainment sector with intuitive and convenient-to-use solutions, including VOD and OTT streaming tools, broadcasting services, etc."
+        desc: "Strawberry Labs provides its customers from the Media & Entertainment sector with intuitive and convenient-to-use solutions, including VOD and OTT streaming tools, broadcasting services, etc.",
+        icon: FaFilm
     },
     {
         title: "Telecom",
-        desc: "Strawberry Labs' dedicated Telecom software developers and other experts are ready to help you with such solutions as OSS/BSS, telecom-specific web and mobile apps, VoIP projects, etc."
+        desc: "Strawberry Labs' dedicated Telecom software developers and other experts are ready to help you with such solutions as OSS/BSS, telecom-specific web and mobile apps, VoIP projects, etc.",
+        icon: FaPhone
     },
     {
         title: "Logistics Software",
-        desc: "Strawberry Labs' custom software development expertise in Logistics, Supply Chain, Transportation, and Warehousing enables us to handle projects of a wide scope, giving you leverage to grow and succeed."
+        desc: "Strawberry Labs' custom software development expertise in Logistics, Supply Chain, Transportation, and Warehousing enables us to handle projects of a wide scope, giving you leverage to grow and succeed.",
+        icon: FaTruck
     },
     {
         title: "eCommerce Solutions",
-        desc: "With our knowledge of the contemporary eCommerce landscape and hands-on skills tested on challenging products, we can deliver advanced solutions that drive revenue and increase sales."
+        desc: "With our knowledge of the contemporary eCommerce landscape and hands-on skills tested on challenging products, we can deliver advanced solutions that drive revenue and increase sales.",
+        icon: FaSellsy
     }
 ]
 
-function IndustryCard({ text1, text2 }: { text1: string, text2: string }) {
+function IndustryCard({ text1, text2, icon }: { text1: string, text2: string, icon: IconType }) {
     return (
         <Box px={4} my={1}>
             <VStack shadow={"base"} align={'start'} p={6} _hover={{ shadow: "2xl" }}>
+                <Icon as={icon} boxSize={'5'} />
                 <Text fontSize={'2xl'}>{text1}</Text>
                 <Text>{text2}</Text>
                 <Button rightIcon={<ArrowForwardIcon />} variant={'Link'} _hover={{ textColor: "#ba0236" }}>See more</Button>
@@ -59,12 +69,12 @@ export default function Section8() {
             </Box>
             <Box display={{ base: 'none', lg: 'block' }} >
                 <SimpleSlider slidesToShow={4} horizontalMargin={12} arrows={true}>
-                    {data.map(item => (<IndustryCard text1={item.title} text2={item.desc} />))}
+                    {data.map(item => (<IndustryCard text1={item.title} text2={item.desc} icon={item.icon} />))}
                 </SimpleSlider>
             </Box>
             <Box display={{ base: 'block', lg: 'none' }} >
                 <SimpleSlider slidesToShow={1} horizontalMargin={4} arrows={false}>
-                    {data.map(item => (<IndustryCard text1={item.title} text2={item.desc} />))}
+                    {data.map(item => (<IndustryCard text1={item.title} text2={item.desc} icon={item.icon} />))}
                 </SimpleSlider>
             </Box>
         </Box>
